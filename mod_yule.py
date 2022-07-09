ecbd4151ba7be33dd2a9fed564d4e1e79ff17ef7
Adam Gorske <adamgorske@gmail.com>
2022-07-09 10:20:10 -0700

    first commit

---
+++ mod_yule.py
@@ -0,0 +1,11 @@
+# square footage of a house
+from math import pi
+
+
+def sqft(length, width):
+    return length * width
+
+
+# circumference of a circle
+def circum_circ(radius):
+    return round(2*pi*radius, 2)
